57dee4a1f73b77bf2c6c4ccea6ca31b176cb0965
phil <philiptgjackson@gmail.com>
2021-08-11 13:07:23 +0100

    feat: standalone appbar

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,11 +1,59 @@
-import React, { ReactElement } from "react";
+import { ReactElement } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Grid,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
+import { svgSrc } from "@/helpers";
+import { theme } from "@gliff-ai/style";
+
+const useStyles = makeStyles(() => ({
+  appBar: {
+    backgroundColor: theme.palette.secondary.light,
+    height: "90px",
+    paddingTop: "9px",
+  },
+  logo: {
+    marginBottom: "5px",
+    marginTop: "7px",
+  },
+}));
 
 interface Props {
   showAppBar?: boolean;
 }
 
 const UserInterface = (props: Props): ReactElement => {
-  return <h1>Hello, World!</h1>;
+  const { showAppBar } = props;
+  const classes = useStyles();
+
+  const appBar = !showAppBar ? null : (
+    <AppBar position="fixed" className={classes.appBar} elevation={0}>
+      <Toolbar>
+        <Grid container direction="row">
+          <Grid item className={classes.logo}>
+            <img
+              src={svgSrc("gliff-master-black")}
+              width="79px"
+              height="60px"
+              alt="gliff logo"
+            />
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      {appBar}
+      <div style={{ marginTop: showAppBar ? "108px" : "20px" }}>
+        <h1>Hello, World!</h1>
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default UserInterface;
