d485de31df3970634e4fa24a4c3b3fb9f7172d54
phil <philiptgjackson@gmail.com>
2021-08-23 13:09:49 +0100

    chore: move annotation audit table out to components dir

--- src/ui.tsx
+++ src/ui.tsx
@@ -8,15 +8,6 @@ import {
   Card,
   Paper,
   Typography,
-  TableContainer,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
   TextField,
 } from "@material-ui/core";
 import { theme } from "@gliff-ai/style";
@@ -24,6 +15,7 @@ import { AuditAction } from "@gliff-ai/annotate";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { svgSrc } from "@/components/helpers";
 import { SearchBar } from "@/components/SearchBar";
+import { AnnotationAuditTable } from "@/components/AnnotationAuditTable";
 
 const useStyles = makeStyles(() => ({
   input1: {
@@ -124,66 +116,11 @@ const UserInterface = (props: Props): ReactElement => {
               Audit Trail
             </Typography>
           </Paper>
-          <TableContainer>
-            <Table aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell className={classes.tableText}>
-                    Date &amp; Time
-                  </TableCell>
-                  <TableCell className={classes.tableText}>Action</TableCell>
-                  <TableCell className={classes.tableText}>Details</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {props.audit
-                  .filter(
-                    (action: AuditAction) =>
-                      !["Action", "Details"].includes(searchField) ||
-                      (searchField === "Action" &&
-                        action.method
-                          .toLowerCase()
-                          .includes(searchValue.toLowerCase())) ||
-                      (searchField === "Details" &&
-                        action.args.toLowerCase().includes(searchValue))
-                  )
-                  .map((action: AuditAction) => (
-                    <TableRow key={`${action.timestamp} + ${action.method}`}>
-                      <TableCell>
-                        {new Date(action.timestamp).toLocaleString()}
-                      </TableCell>
-                      <TableCell>{action.method}</TableCell>
-                      <TableCell
-                        style={{
-                          width: "30%",
-                          paddingTop: "0px",
-                          paddingBottom: "0px",
-                        }}
-                      >
-                        <Accordion>
-                          <AccordionSummary
-                            expandIcon={<ExpandMoreIcon />}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
-                          >
-                            <Typography>
-                              {action.args.length > 32
-                                ? action.args.substr(0, 32).concat("...")
-                                : action.args}
-                            </Typography>
-                          </AccordionSummary>
-                          <AccordionDetails>
-                            <Typography style={{ whiteSpace: "pre-wrap" }}>
-                              {JSON.stringify(JSON.parse(action.args), null, 2)}
-                            </Typography>
-                          </AccordionDetails>
-                        </Accordion>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+          <AnnotationAuditTable
+            audit={props.audit}
+            searchField={searchField}
+            searchValue={searchValue}
+          />
         </Card>
       </div>
     </ThemeProvider>
